cc02c267398e636d3ec89fa1a9e22f242394802d
Renaldo Freire <renaldo@disroot.org>
2023-01-14 02:04:49 -0300

    Qtile: container jellyfin settings

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -10,7 +10,8 @@ clementine &
 crow &
 firefox &
 alacritty &
-xset r rate 400 30
+~/docker/jellyfin/ docker-compose up -d;
+xset r rate 401 30
 light-locker --lock-after-screensaver=600 --lock-on-suspend & #lighDM screen locker
 #setxkbmap -layout us -variant intl
 
