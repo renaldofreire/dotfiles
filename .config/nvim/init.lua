3a0ca2ff7ce685167f2bbdf151cbbc72f96e5d82
Renaldo Freire <eurenaldo@gmail.com>
2023-10-13 00:59:02 -0300

    adcionar nvim cofig

---
+++ .config/nvim/init.lua
@@ -0,0 +1,1 @@
+require("refnvim") -- chama pasta padrão
---
+++ .config/nvim/lua/refnvim/init.lua
@@ -0,0 +1,6 @@
+require("refnvim.remap")
+require("refnvim.set")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
