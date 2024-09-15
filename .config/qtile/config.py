aacea327de38ca9d654915aeb478b37c6428671b
Renaldo Freire <orenaldo@proton.me>
2024-09-15 00:43:46 -0300

    in groups, replace match using re

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,30 +1,5 @@
-# COPYRIGHT (C) 2010 ALDO CORTESI
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import os
+import re
 import subprocess
 from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
@@ -98,16 +73,16 @@ keys = [
 
 # groups = [Group(i) for i in "1234567890"] -> default
 groups = [
-    Group("1", matches=[Match(wm_class=["Navigator", "firefox", "floorp"])]),
-    Group("2", matches=[Match(wm_class=["alacritty"])]),
-    Group("3", matches=[Match(wm_class=["Joplin"])]),
+    Group("1", matches=[Match(wm_class=re.compile(r"^(Navigator|firefox)$"))]),
+    Group("2", matches=[Match(wm_class=re.compile(r"^(alacritty)$"))]),
+    Group("3", matches=[Match(wm_class=re.compile(r"^(Joplin)$"))]),
     Group("4"),
     Group("5"),
     Group("6"),
-    Group("7", matches=[Match(wm_class=["pocket-casts", "strawberry", "easyeffects"])]),
+    Group("7", matches=[Match(wm_class=re.compile(r"^(pocket-casts|strawberry|easyeffects)$"))]),
     Group("8"),
-    Group("9", matches=[Match(wm_class=["signal", "element", "telegram-desktop"])]),
-    Group("0", matches=[Match(wm_class=["Mail", "thunderbird"])]),
+    Group("9", matches=[Match(wm_class=re.compile(r"^(signal|element|telegram-desktop)$"))]),
+    Group("0", matches=[Match(wm_class=re.compile(r"^(Mail|thunderbird)$"))]),
 ]
 
 for i in groups:
@@ -222,6 +197,7 @@ mouse = [
 def autostart():
     home = os.path.expanduser("~/.config/qtile/scripts/autostart.sh")
     subprocess.Popen([home])
+
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
