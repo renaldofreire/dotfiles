f90ecb78abe7745be6ca2dc504d85c86de004210
Renaldo Freire <orenaldo@proton.me>
2024-09-15 00:54:18 -0300

    only change aplications name positions

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -3,19 +3,19 @@
 picom &
 nitrogen --restore
 dropbox &
-#megasync &
 caffeine &
 nm-applet &
 sxhkd &
-# strawberry &
 crow &
-# mullvad-vpn &
-# librewolf &
 firefox &
 alacritty &
-#qbittorrent &
 xset r rate 401 30
 light-locker --lock-after-screensaver=600 --lock-on-suspend & #lighDM screen locker
 sleep 100 && qtile cmd-obj -o cmd -f reload_config
+#megasync &
+# strawberry &
+# mullvad-vpn &
+# librewolf &
+#qbittorrent &
 #setxkbmap -layout us -variant intl
 
