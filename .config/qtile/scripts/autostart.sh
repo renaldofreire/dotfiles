602aac0e6b072bea9b70d830537555c0ce7741b2
Renaldo Freire <orenaldo@proton.me>
2024-12-26 14:09:55 -0300

    some changes in autostart apps

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -8,12 +8,12 @@ nm-applet &
 sxhkd &
 crow &
 zen-browser &
-mullvad-vpn &
 notesnook &
 alacritty &
 xset r rate 401 30
 light-locker --lock-after-screensaver=600 --lock-on-suspend & #lighDM screen locker
 sleep 100 && qtile cmd-obj -o cmd -f reload_config
+#mullvad-vpn &
 #firefox &
 #megasync &
 # strawberry &
